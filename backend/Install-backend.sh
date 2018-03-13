268a9f358b6e594d4baa22db3b2d9604bbd98554
nishalpereira <nishalpereira@gmail.com>
2018-03-13 14:20:42 +0530

    run tmux session on boot-up

--- backend/Install-backend.sh
+++ backend/Install-backend.sh
@@ -6,3 +6,6 @@ sudo npm install
 sudo npm install csv-parse
 sudo npm install request
 sudo apt-get install tmux
+sudo cp rc.local /etc/rc.local
+sudo sed -i -e "s|home_path|$HOME|" /etc/rc.local 
+sudo chmod +x /etc/rc.local
